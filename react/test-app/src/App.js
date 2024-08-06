1b5f57483124db71b21107ba73d8ebd93cbb1b6b
aghaith <abbasghayth5@gmail.com>
2024-08-06 19:51:18 +0400

    Props

--- react/test-app/src/App.js
+++ react/test-app/src/App.js
@@ -2,9 +2,20 @@ import "./App.css";
 
 export default function App() {
   return (
-    <>
+    <Comment username="Abbas" time={new Date().toString()}>
       <h1>Hello World</h1>
-      <p>Test</p>
-    </>
+      <p>This is a comment</p>
+    </Comment>
+  );
+}
+
+function Comment({ username, time, children }) {
+  return (
+    <section>
+      <p>
+        {username} commented at {time}
+      </p>
+      {children}
+    </section>
   );
 }
